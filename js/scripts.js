00a2588f4fa990b7f3c19b2bdeba6298c56d0788
FSArriagada <facu._99@hotmail.com>
2022-05-05 19:29:34 -0300

    El carro paso de ser un array a un objeto

--- js/scripts.js
+++ js/scripts.js
@@ -4,32 +4,41 @@ class Telefono{
         this.modelo = modelo
         this.precio = precio
     }
+}
+        
+
+class Carro {
+    constructor(total){
+        this.telefonos = []
+        this.total = total
+    }
+    agregarAlCarro (celular){
+        this.telefonos.push(celular)
+        this.total += celular.precio
+    }
     
+    MostrarCarro(){
+        this.telefonos.forEach(listado => {
+            console.log(listado);
+        });
+    }
 }
 
-let Carro = [];        
-
-function agregarAlCarro(celular) {
-    Carro.push(celular)
-    alert('Se agregó al carro')
-}
-
-
-let precio
 let modelo
 let marca
-let total = 0
 let seguir = true
 
 const celular = new Telefono("Samsung","A32",41999)
 const celular2 = new Telefono("Samsung","A52",52999)
 const celular3 = new Telefono("Samsung","A72",64999)
-const celular4 = new Telefono("Apple","Iphone SE",109999)
-const celular5 = new Telefono("Apple","Iphone 11",142999)
-const celular6 = new Telefono("Apple","Iphone 12",189999)
-const celular7 = new Telefono("Xiaomi","Note 10",59999)
-const celular8 = new Telefono("Xiaomi","Note 11",66999)
-const celular9 = new Telefono("Xiaomi","Poco M3",69999)
+const celular4 = new Telefono("Apple","iphone SE",109999)
+const celular5 = new Telefono("Apple","iphone 11",142999)
+const celular6 = new Telefono("Apple","iphone 12",189999)
+const celular7 = new Telefono("Xiaomi","note 10",59999)
+const celular8 = new Telefono("Xiaomi","note 11",66999)
+const celular9 = new Telefono("Xiaomi","poco m3",69999)
+const carro = new Carro(0);
+
 // Ciclo while para preguntar si el usuario quiere seguir comprando o no
 while (seguir) {
     marca = prompt("Ingrese la marca del celular. (Samsung, Apple, Xiaomi)").toLowerCase()
@@ -48,21 +57,18 @@ while (seguir) {
             switch(modelo){
                 case "a32":
                     alert("Usted eligió Samsung A32 (Precio: $41999)")
-                    Sumar(41999)
-                    agregarAlCarro(celular)
-                    break;
+                    carro.agregarAlCarro(celular)
+                break;
                 case "a52":
                     alert("Usted eligió Samsung A52 (Precio: $52999)")
-                    agregarAlCarro(celular2)
-                    Sumar(52999)
-                    break;
+                    carro.agregarAlCarro(celular2)  
+                break;
                 case "a72":
                     alert("Usted eligió Samsung A72 (Precio: $64999)")
-                    agregarAlCarro(celular3)
-                    Sumar(64999)
-                    break;
+                    carro.agregarAlCarro(celular3)
+                break;
             }
-            break;
+        break;
         case "apple":
             modelo = prompt("Elija el modelo del producto. SE, 11, 12").toLowerCase()
             while ((modelo != "se") && (modelo != "11") && (modelo != "12")){
@@ -72,21 +78,18 @@ while (seguir) {
             switch(modelo){
                 case "se":
                     alert("Usted eligió Iphone SE (Precio: $109999)")
-                    agregarAlCarro(celular4)
-                    Sumar(109999)
-                    break;
+                    carro.agregarAlCarro(celular4)
+                break;
                 case "11":
                     alert("Usted eligió Iphone 11 (Precio: $142999)")
-                    agregarAlCarro(celular5)
-                    Sumar(142999)
-                    break;
+                    carro.agregarAlCarro(celular5)
+                break;
                 case "12":
                     alert("Usted eligió Iphone 12 (Precio: $189999)")
-                    agregarAlCarro(celular6)
-                    Sumar(189999)
-                    break;
+                    carro.agregarAlCarro(celular6)
+                break;
             }
-            break;
+        break;
         case "xiaomi":
             modelo = prompt("Elija el modelo del producto. Note 10, Note 11, Poco M3").toLowerCase()
             while ((modelo != "note 10") && (modelo != "note 11") && (modelo != "poco m3")){
@@ -95,37 +98,28 @@ while (seguir) {
             }
             switch(modelo){
                 case ("note 10"):
-                    alert("Usted eligió Xiaomi Note 10(Precio: $59999)")
-                    agregarAlCarro(celular7)
-                    Sumar(59999)
-                    break;
+                    alert("Usted eligió Xiaomi Note 10 (Precio: $59999)")
+                    carro.agregarAlCarro(celular7)
+                break;
                 case ("note 11"):
-                    alert("Usted eligió Xiaomi Note 11(Precio: $66999)")
-                    agregarAlCarro(celular8)
-                    Sumar(66999)
-                    break;
+                    alert("Usted eligió Xiaomi Note 11 (Precio: $66999)")
+                    carro.agregarAlCarro(celular8)
+                break;
                 case ("poco m3"):
-                    alert("Usted eligió Xiaomi Poco M3(Precio: $69999)")
-                    agregarAlCarro(celular9)
-                    Sumar(69999)
-                    break;
+                    alert("Usted eligió Xiaomi Poco M3 (Precio: $69999)")
+                    carro.agregarAlCarro(celular9)
+                break;
             }
-            break;
+        break;
        
     }
     let opcion = prompt("¿Desea seguir comprando? Si/No").toLowerCase()
     if (opcion == "no"){
         seguir = false;
         break
-    } 
+    }  
 }
-//Funcion para sumar el precio al total 
-function Sumar(precio){
-    total +=precio;
-    return total
-}
+
 alert(`Ver consola para ver la lista de productos`)
-Carro.forEach(listado => {
-    console.log(listado);
-});
-alert(`El total de su compra es de: ${total}`)
+carro.MostrarCarro()
+alert(`El total de su compra es de: ${carro.total}`)
